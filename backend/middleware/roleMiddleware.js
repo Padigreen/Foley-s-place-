c01dfd5cb232ee521528561c688a64adb48bf189
Padigreen <adejohsolomonoliver@gmail.com>
2025-06-13 20:32:14 +0100

    Delete backend directory

--- backend/middleware/roleMiddleware.js
+++
@@ -1,9 +0,0 @@
-function authorizeRoles(...allowedRoles) {
-  return (req, res, next) => {
-    if (!req.user || !allowedRoles.includes(req.user.role)) {
-      return res.status(403).json({ message: "Forbidden: insufficient permissions" });
-    }
-    next();
-  };
-}
-module.exports = authorizeRoles;
